3289fe949e950a746e076df681563af4d619996f
Morgan McDermott <morgan@morganmcdermott.com>
2014-03-01 11:45:42 -0800

    Init

---
+++ randomForestTest.py
@@ -0,0 +1,14 @@
+from util import *
+
+filters = [crop((150, 275), (150, 275)), resize(36, 36), grayscale]
+(X, Y) = loadProcess(0.03, filters)
+
+preview1D(X[0], 36, 36)
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+     X, Y, test_size=0.4, random_state=0)
+
+clf = RandomForestRegressor(n_estimators=5, n_jobs=-1)
+clf = clf.fit(X_train, y_train)
+score = clf.score(X_test, y_test)  
+print score
---
+++ util.py
@@ -0,0 +1,67 @@
+import cv2
+import os
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn import cross_validation
+
+imageRoot  ='images_training_rev1/'
+dataFile = 'training_solutions_rev1.csv'
+
+# Load CSV data into a hashmap keyed on the first value
+def loadData():
+    data = np.genfromtxt(dataFile, delimiter=',')
+    dataHM = {}
+    for i in xrange(1, len(data)):  dataHM[ int(data[i][0]) ] = data[i][1:]
+    return dataHM
+
+def loadImages(ratio):
+    images = os.listdir(imageRoot)
+    n = int(len(images) * ratio)
+    print "Loading " + str(n) + " Images"
+    loaded = []
+    for i in xrange(0, n):
+        if i % 1000 == 0: print ".",
+        loaded.append({'ID': int(images[i].split(".")[0])
+                       ,"img": cv2.imread(imageRoot + images[i]) })
+    return loaded
+
+# Sequentially apply transformations to the images        
+def preprocess(images, filters):
+    print "\nPreprocessing images"
+    processed = []
+    for i in xrange(0, len(images)):
+        if i % 1000 == 0: print ".",
+        image = images[i]
+        for j in xrange(0, len(filters)): 
+            image['img'] = filters[j](image['img'])
+        processed.append(image)
+    return processed
+
+# Some convenience filters
+def crop((x1, x2), (y1, y2)): return (lambda img: img[x1:x2, y1:y2])
+def resize(x, y): return (lambda img: cv2.resize(img, (x, y)))
+def grayscale(img): return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Convert CSV data and image data to 1D data and targets
+def combineData(dataHM, processed):
+    X = []
+    Y = []
+    for i in xrange(0, len(processed)): 
+        x = processed[i]['img']
+        X.append( x.reshape(x.shape[0] * x.shape[1]) )
+        Y.append(dataHM[processed[i]['ID']])
+    return (X, Y)
+    
+# Do everything above
+def loadProcess(ratioImages, filters):
+    dataHM = loadData()
+    loaded = loadImages(ratioImages)
+    processed = preprocess(loaded, filters)
+    return combineData(dataHM, processed)
+
+# view a 1D array as an image
+def preview1D(image, x, y):
+    img = image.reshape(x, y)
+    cv2.imshow("Sample", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
